0c718946a06a901481a2f0de70876e0f44d797ad
ana-vela <ana@anavela.net>
2021-01-11 20:27:38 -0700

    Add search function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+// import { getSuggestedQuery } from "@testing-library/react";
 
 function App() {
-  const [ingredient, updateIngredient] = useState([]);
+  const [query, setQuery] = useState("");
+  const [ingredients, updateIngredients] = useState([]);
 
-  useEffect(() => {
-    let url = `https://api.edamam.com/search?q=chicken&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`;
+  const searchIngredients = async (e) => {
+    e.preventDefault();
+    let url = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}`;
     fetch(url)
       .then((response) => {
         return response.json();
       })
       .then((res) => {
         console.log(res.hits);
-        updateIngredient(res.hits);
+        updateIngredients(res.hits);
       })
       .catch((err) => {
         console.log("error", err);
       });
-  }, []);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="Wrapper">
-          {ingredient.map((item) => {
+          <div>
+            <form className="form" onSubmit={searchIngredients}>
+              <label className="label" htmlFor="query">
+                update Ingredient
+              </label>
+              <input
+                className="input"
+                type="text"
+                name="query"
+                placeholder="eggs"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <button className="button" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+          {ingredients.map((ingredient) => {
             return (
               <div className="Card">
-                <span>{item.recipe.label}</span>
-                <img alt="recipe" src={item.recipe.image} />
-                {item.recipe.ingredientLines.map((step) => (
+                <span>{ingredient.recipe.label}</span>
+                <img alt="recipe" src={ingredient.recipe.image} />
+                {ingredient.recipe.ingredientLines.map((step) => (
                   <p>{step}</p>
                 ))}
                 <p>
-                  Source:<a href={item.recipe.url}>{item.recipe.source}</a>
+                  Source:
+                  <a href={ingredient.recipe.url}>{ingredient.recipe.source}</a>
                 </p>
               </div>
             );
